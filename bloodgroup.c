8a60c6743a604657b1b2b6c9ec48ca6fa704427a
moominminmoo <mo.morinee@gmail.com>
2022-09-23 08:53:39 +0700

    change A 2 places

--- bloodgroup.c
+++ bloodgroup.c
@@ -23,12 +23,12 @@ void main()
                             strcmp(G, "O") !=0 );
     if (strcmp(G,"A") ==0)
     {
-      printf("%s, A. Hey, you can give blood to: A, AB.\n", Name);
+      printf("%s, A. Hey go, you can give blood to: A, AB.\n", Name);
       printf("  You can receive blood from: A, O.\n");
     }
     else if (strcmp(G, "B") ==0)
     {
-      printf("%s, B. Well, you can give blood to: B, AB.\n", Name);
+      printf("%s, B. Well done, you can give blood to: B, AB.\n", Name);
       printf("  You can receive blood from: B, O.\n");
     }
     else if (strcmp(G, "AB") == 0)
